2255b515ae0a4afa9e0e3fe0f274380812a2e5d4
daasol <dasol806@kpu.ac.kr>
2021-12-13 14:17:08 +0900

    Modify Config.java - Add comment (sender and receiver account, mail server)

--- src/config/Config.java
+++ src/config/Config.java
@@ -5,19 +5,19 @@ public class Config {
 	/**
 	 * Mail Server
 	 * **/
-	public static String HOST="mail.tmax.co.kr";
+	public static String HOST=""; // mail server
 	public static String PORT="25";
 	public static String TLSVERSION="TLSv1.2";
 	public static String SENDERID="no_reply.wapl@tmax.co.kr";
 	//	public static String SENDERID="no_reply.wapl@tmax.co.kr";
-	public static String SENDERPW="Miracle!";
-	public static String RECEIVER="dasol_hwang@tmax.co.kr";
+	public static String SENDERPW=""; // sender account
+	public static String RECEIVER=""; // receiver account
 	
 	/**
 	 * Mail Title and Body
 	 * **/
-	public static String TITLE="test";
-	public static String BODY="test";
+	public static String TITLE="test"; // mail title
+	public static String BODY="test";  // mail body
  
 
 }
